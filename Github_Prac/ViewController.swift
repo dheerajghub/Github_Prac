75362e4d47b7b113adcf8d9246455a3b64807a8a
dheeraj <dheerajsh123456@gmail.com>
2020-02-09 08:24:39 +0530

    Json Parsing in swift 2/3 and obj-c

--- Github_Prac/ViewController.swift
+++ Github_Prac/ViewController.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
+struct githubUser{
+    
+    var avatarUrl:String!
+    var bio:String!
+    var blog:String!
+    var followers:Int!
+    var following:Int!
+    
+    init(json:[String:Any]){
+        avatarUrl = json["avatar_url"] as? String ?? ""
+        bio = json["bio"] as? String ?? ""
+        blog = json["blog"] as? String ?? ""
+        followers = json["followers"] as? Int ?? -1
+        following = json["following"] as? Int ?? -1
+    }
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -17,15 +35,27 @@ class ViewController: UIViewController {
     
     func fetchGithubUser(){
         
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.github.com"
-        urlComponents.path = "/users/dheerajghub"
-        guard let url = urlComponents.url else {
-            preconditionFailure("Failed to construct URL")
-        }
+        let urlString = "https://api.github.com/users/dheerajghub"
+        guard let url = URL(string: urlString) else { return }
+        
         let task = URLSession.shared.dataTask(with: url){
             data, response, error in
+            
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async {
+                
+                do{
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] else { return }
+                    
+                    let user = githubUser(json: json)
+                    print(user.avatarUrl ?? "")
+                    
+                } catch let jsonErr{
+                    print("Error JSON serializing:" , jsonErr)
+                }
+            }
+            
         }
         task.resume()
         
