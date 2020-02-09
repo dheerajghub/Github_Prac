4c18056208a49e5bd13d352c5080eb8ec643bf97
dheeraj <dheerajsh123456@gmail.com>
2020-02-09 07:40:38 +0530

    gitignore added

--- git.py
+++ git.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--msg', 
     help="shows output")
-args = parser.parse_args() 
+args = parser.parse_args()
 os.system("git add .")
 os.system('git commit -m "'+args.msg+'"')
 os.system("git push -u origin master")
